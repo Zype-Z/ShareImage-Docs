657d0e44374a3a2b6e1b494b9f9f7186383a8c00
Rajdeep Malakar <Rajdeep@tgeeks.cf>
2021-09-24 16:18:41 +0530

    Remove Unused Pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
-import 'nextra-theme-docs/style.css'
+import 'nextra-theme-docs/style.css';
+import Prism from 'prism-react-renderer/prism'
+
+(typeof global !== "undefined" ? global : window).Prism = Prism
+
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-jsx';
+import 'prismjs/components/prism-js-extras';
+import 'prismjs/components/prism-python';
+import 'prismjs/components/prism-typescript';
+import 'prismjs/components/prism-tsx';
+import 'prismjs/components/prism-shell-session';
+import 'prismjs/components/prism-json';
+import 'prismjs/components/prism-bash';
 
 export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return <Component {...pageProps }
+    />
 }
